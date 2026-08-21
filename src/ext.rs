420c27c25d521cd7950de2fc5461ead11d6b8d3a
Patrick Owen <patowen95@gmail.com>
2026-08-21 00:22:27 -0400

    Move unit tests to colocate them with the code they're testing

--- src/ext.rs
+++ src/ext.rs
@@ -7,3 +7,26 @@ use crate::small_float;
 pub fn min_allocator_size(needed_object_size: u32) -> u32 {
     small_float::float_to_uint(small_float::uint_to_float_round_up(needed_object_size))
 }
+
+#[cfg(test)]
+mod tests {
+    use crate::Allocator;
+
+    use super::*;
+
+    #[test]
+    fn ext_min_allocator_size() {
+        // Randomly generated integers on a log distribution, σ = 10.
+        static TEST_OBJECT_SIZES: [u32; 42] = [
+            0, 1, 2, 3, 4, 5, 8, 17, 23, 36, 51, 68, 87, 151, 165, 167, 201, 223, 306, 346, 394,
+            411, 806, 969, 1404, 1798, 2236, 4281, 4745, 13989, 21095, 26594, 27146, 29679, 144685,
+            153878, 495127, 727999, 1377073, 9440387, 41994490, 68520116,
+        ];
+
+        for needed_object_size in TEST_OBJECT_SIZES {
+            let allocator_size = min_allocator_size(needed_object_size);
+            let mut allocator: Allocator<u32> = Allocator::new(allocator_size);
+            assert!(allocator.allocate(needed_object_size).is_some());
+        }
+    }
+}
--- src/small_float.rs
+++ src/small_float.rs
@@ -77,3 +77,91 @@ pub fn float_to_uint(float_value: u32) -> u32 {
         (mantissa | MANTISSA_VALUE) << (exponent - 1)
     }
 }
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn small_float_uint_to_float() {
+        // Denorms, exp=1 and exp=2 + mantissa = 0 are all precise.
+        // NOTE: Assuming 8 value (3 bit) mantissa.
+        // If this test fails, please change this assumption!
+        let precise_number_count = 17;
+        for i in 0..precise_number_count {
+            let round_up = uint_to_float_round_up(i);
+            let round_down = uint_to_float_round_down(i);
+            assert_eq!(i, round_up);
+            assert_eq!(i, round_down);
+        }
+
+        // Test some random picked numbers
+        struct NumberFloatUpDown {
+            number: u32,
+            up: u32,
+            down: u32,
+        }
+
+        let test_data = [
+            NumberFloatUpDown {
+                number: 17,
+                up: 17,
+                down: 16,
+            },
+            NumberFloatUpDown {
+                number: 118,
+                up: 39,
+                down: 38,
+            },
+            NumberFloatUpDown {
+                number: 1024,
+                up: 64,
+                down: 64,
+            },
+            NumberFloatUpDown {
+                number: 65536,
+                up: 112,
+                down: 112,
+            },
+            NumberFloatUpDown {
+                number: 529445,
+                up: 137,
+                down: 136,
+            },
+            NumberFloatUpDown {
+                number: 1048575,
+                up: 144,
+                down: 143,
+            },
+        ];
+
+        for v in test_data {
+            let round_up = uint_to_float_round_up(v.number);
+            let round_down = uint_to_float_round_down(v.number);
+            assert_eq!(round_up, v.up);
+            assert_eq!(round_down, v.down);
+        }
+    }
+
+    #[test]
+    fn small_float_float_to_uint() {
+        // Denorms, exp=1 and exp=2 + mantissa = 0 are all precise.
+        // NOTE: Assuming 8 value (3 bit) mantissa.
+        // If this test fails, please change this assumption!
+        let precise_number_count = 17;
+        for i in 0..precise_number_count {
+            let v = float_to_uint(i);
+            assert_eq!(i, v);
+        }
+
+        // Test that float->uint->float conversion is precise for all numbers
+        // NOTE: Test values < 240. 240->4G = overflows 32 bit integer
+        for i in 0..240 {
+            let v = float_to_uint(i);
+            let round_up = uint_to_float_round_up(v);
+            let round_down = uint_to_float_round_down(v);
+            assert_eq!(i, round_up);
+            assert_eq!(i, round_down);
+        }
+    }
+}
